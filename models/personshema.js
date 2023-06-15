1db809ee39882cff9101f45c359c53df2942be0d
ritikjain1234 <ritikcool45@gmail.com>
2023-06-15 23:49:19 +0530

    add joi

--- models/personshema.js
+++ models/personshema.js
@@ -1,21 +1,41 @@
 const mongoose = require("mongoose");
+const joi = require("joi");
+const joigoose = require("joigoose")(mongoose);
+ 
+//normal Schema without using joi
+// // const personInfo = new mongoose.Schema({
+// //     Name:{
+// //         type:String,
+// //         trim:true,
+// //     },
+// //     Id:{
+// //         type:Number,
+// //         required:true,
+// //         unique:true
+// //     },
+// //     City:{
+// //         type:String,
+// //         default:"Agra"
+// //     }
+// // });
 
-const personInfo = new mongoose.Schema({
-    Name:{
-        type:String,
-        trim:true,
-    },
-    Id:{
-        type:Number,
-        required:true,
-        unique:true
-    },
-    City:{
-        type:String,
-        default:"Agra"
-    }
+// // const PersonData = new mongoose.model("personData",personInfo);
+
+// module.exports = PersonData;
+
+
+// Schema with using joi
+const personInfo = joi.object({
+    Name:joi.string().trim(),
+    Id:joi.number(),
+    City:joi.string()
 });
 
-const PersonData = new mongoose.model("personData",personInfo);
+const joiSchema = new mongoose.Schema(
+    joigoose.convert(personInfo)
+);
+
+
+const PersonData = new mongoose.model("personData",joiSchema);
 
 module.exports = PersonData;
